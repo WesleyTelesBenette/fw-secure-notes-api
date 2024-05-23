c5caa253a3ada716ea720e3b9508ff2bd3dd5df0
Wesley T. Benette <weluonf@gmail.com>
2024-05-23 19:11:44 -0300

    🔁 update(PageRepository): Adds IsPageValid method

--- Data/PageRepository.cs
+++ Data/PageRepository.cs
@@ -1,11 +1,22 @@
-﻿namespace fw_secure_notes_api.Data;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace fw_secure_notes_api.Data;
 
 public class PageRepository
 {
     private readonly DatabaseContext _dbContext;
 
     public PageRepository(DatabaseContext dbContext)
-        { _dbContext = dbContext; }
+    {
+        _dbContext = dbContext;
+    }
 
-
+    public async Task<bool> IsPageValid(string title, string pin, string password)
+    {
+        var page = await _dbContext.Pages
+            .FirstOrDefaultAsync(p => (p.Title == title) && (p.Pin == pin));
+        
+        return (page != null) && BCrypt.Net.BCrypt.Verify(password, page.Password);
+    }
 }
+ 
