c0f263eb795216bdc76635f801b077624c3b04fa
Wesley T. Benette <weluonf@gmail.com>
2024-05-23 10:26:41 -0300

    ✨ feat: Create Models
    - PageModel
    - FileModel
    
    Refs #FWSN-4

---
+++ Models/FileModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace fw_secure_notes_api.Models;
+
+[Table("File")]
+public class FileModel
+{
+    [Key]
+    public ushort Id { get; set; }
+
+    [MaxLength(24)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    public string Content { get; set; } = string.Empty;
+
+    [Required]
+    [ForeignKey(nameof(FileModel.Page))]
+    public uint PageId { get; set; }
+
+    public PageModel Page { get; set; }
+
+    public FileModel() { }
+}
---
+++ Models/PageModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace fw_secure_notes_api.Models;
+
+public class PageModel
+{
+    [Key]
+    public uint Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    [Required]
+    public string Pin { get; set; }
+
+    public string Password { get; set; } = "";
+
+    public ThemePage Theme { get; set; } = ThemePage.Dark;
+
+    public ICollection<FileModel> Files { get; set; } = [];
+
+    public PageModel() { }
+}
+
+public enum ThemePage
+{
+    Dark,
+    Light,
+    Red,
+    Gold,
+    Blue,
+    Green,
+    Purple,
+    Orange
+}
