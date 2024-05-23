aaa97708952d4a4dd7bdc87666addba12690fc29
Wesley T. Benette <weluonf@gmail.com>
2024-05-23 19:12:01 -0300

    ✨ feat: Creates Login DTO

---
+++ Dtos/LoginDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fw_secure_notes_api.Dtos;
+
+public class LoginDto
+{
+    [Required]
+    [MaxLength(25)]
+    public string Title { get; set; }
+
+    [Required]
+    [MaxLength(3)]
+    [RegularExpression(@"^[a-zA-Z0-9\-]*$")]
+    public string Pin { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
