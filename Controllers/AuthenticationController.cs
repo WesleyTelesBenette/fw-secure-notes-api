c15be826b1ffbfc0401a3118f46b5a3494624cde
Wesley T. Benette <weluonf@gmail.com>
2024-05-24 11:43:29 -0300

    ✨ feat: Creates Route Validation Middleware
    Requires all requests to have the following parameters:
    - title
    - pin
    
    Refs #FWSN-10

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -10,7 +10,7 @@ using System.Text;
 namespace fw_secure_notes_api.Controllers;
 
 [ApiController]
-[Route("[controler]")]
+[Route("{title}/{pin}/[controller]")]
 public class AuthenticationController : Controller
 {
     private readonly PageRepository _page;
---
+++ Middleware/RouteValidateMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace fw_secure_notes_api.Middleware;
+
+public class RouteValidateMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public RouteValidateMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var routeVars = context.Request.RouteValues;
+
+        routeVars.TryGetValue("title", out var routeTitle);
+        routeVars.TryGetValue("pin",   out var routePin);
+
+        bool isTitleValid = (routeTitle != null) && (routeTitle.ToString()!.Length <= 25);
+        bool isPinValid   = (routePin != null)   && (Regex.IsMatch(routePin.ToString()!, @"^[a-zA-Z0-9\-]*$") == true);
+
+        if ((!isTitleValid) || (!isPinValid))
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsync("Bad Request");
+            context.Abort();
+            return;
+        }
+
+        await _next(context);
+    }
+}
