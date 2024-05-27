560cd6ac636d88f60fa96e9736d928117aebfbb0
Wesley T. Benette <weluonf@gmail.com>
2024-05-27 12:40:50 -0300

    🛠️ refactor (CreatePageDto): Remove imports

--- Dtos/CreatePageDto.cs
+++ Dtos/CreatePageDto.cs
@@ -1,7 +1,4 @@
-﻿using fw_secure_notes_api.Models;
-using System.ComponentModel.DataAnnotations;
-
-namespace fw_secure_notes_api.Dtos;
+﻿namespace fw_secure_notes_api.Dtos;
 
 public class CreatePageDto
 {
