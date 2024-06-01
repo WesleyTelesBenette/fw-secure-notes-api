2a07211ebf2241a78e730c4aad5e90b6ff627233
Wesley T. Benette <weluonf@gmail.com>
2024-06-01 19:26:33 -0300

    🐛 fix(RouteValidateMiddleware): Routes without parameters do not work

--- Middleware/RouteValidateMiddleware.cs
+++ Middleware/RouteValidateMiddleware.cs
@@ -1,4 +1,7 @@
-﻿using System.Text.RegularExpressions;
+﻿using fw_secure_notes_api.Attributes;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
 
 namespace fw_secure_notes_api.Middleware;
 
@@ -13,22 +16,45 @@ public class RouteValidateMiddleware
 
     public async Task Invoke(HttpContext context)
     {
-        var routeVars = context.Request.RouteValues;
 
-        var routeTitle = routeVars.GetValueOrDefault("title");
-        var routePin = routeVars.GetValueOrDefault("pin");
-
-        bool isTitleValid = (routeTitle != null) && (routeTitle.ToString()!.Length <= 25);
-        bool isPinValid   = (routePin   != null) && (Regex.IsMatch(routePin.ToString()!, @"^[a-zA-Z0-9\-]*$"));
-
-        if ((!isTitleValid) || (!isPinValid))
+        if (!VerifyAttribute(context))
         {
-            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            await context.Response.WriteAsync("Bad Request");
-            context.Abort();
-            return;
+            var routeVars = context.Request.RouteValues;
+
+            var routeTitle = routeVars.GetValueOrDefault("title");
+            var routePin = routeVars.GetValueOrDefault("pin");
+
+            bool isTitleValid = (routeTitle != null) && (routeTitle.ToString()!.Length <= 25);
+            bool isPinValid = (routePin != null) && (Regex.IsMatch(routePin.ToString()!, @"^[a-zA-Z0-9\-]*$"));
+
+            if ((!isTitleValid) || (!isPinValid))
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsync("Bad Request");
+                context.Abort();
+                return;
+            }
         }
 
         await _next(context);
+    }
+
+    private static bool VerifyAttribute(HttpContext context)
+    {
+        var endpoint = context.GetEndpoint();
+
+        if (endpoint != null)
+        {
+            var controllerActionDescriptor = endpoint.Metadata
+                .GetMetadata<ControllerActionDescriptor>();
+
+            if (controllerActionDescriptor != null)
+            {
+                return (controllerActionDescriptor.MethodInfo
+                    .GetCustomAttributes(typeof(NoParameters), false).Length != 0);
+            }
+        }
+
+        return false;
     }
 }
