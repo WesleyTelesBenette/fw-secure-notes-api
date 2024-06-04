676f63cb11cc109a94b52b01da4a927571f6df83
Wesley T. Benette <weluonf@gmail.com>
2024-06-04 19:06:29 -0300

    🐛 fix(FileModel): Changes the Content type

---
+++ Dtos/FileModelDto.cs
@@ -0,0 +1,8 @@
+﻿namespace fw_secure_notes_api.Dtos;
+
+public class FileModelDto
+{
+    public string Title { get; set; } = string.Empty;
+
+    public List<string> Content { get; set; } = [];
+}
--- Migrations/20240604173040_UpdateFileModelTypeField.Designer.cs
+++ Migrations/20240604173040_UpdateFileModelTypeField.Designer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using fw_secure_notes_api.Data;
@@ -11,9 +12,11 @@ using fw_secure_notes_api.Data;
 namespace fw_secure_notes_api.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240604173040_UpdateFileModelTypeField")]
+    partial class UpdateFileModelTypeField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
