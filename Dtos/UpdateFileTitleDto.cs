42223673bbc72ae6e594a7ecc863664bb976a5db
Wesley T. Benette <weluonf@gmail.com>
2024-06-14 12:46:59 -0300

    🐛 fix: Allow empty titles

--- Dtos/UpdateFileTitleDto.cs
+++ Dtos/UpdateFileTitleDto.cs
@@ -1,9 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace fw_secure_notes_api.Dtos;
+﻿namespace fw_secure_notes_api.Dtos;
 
 public class UpdateFileTitleDto
 {
-    [Required]
     public string Title { get; set; } = "";
 }
